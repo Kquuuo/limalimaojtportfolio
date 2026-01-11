ed5f2d9c5490276b1305837d92e244a7eeae48e8
unknown <limalimarenatojr@gmail.com>
2026-01-11 23:38:39 +0800

    Changed project images

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -10,35 +10,35 @@ export default function Portfolio() {
       title: "PiJob",
       description: "Built a full-stack e-commerce application with Python, HTML/CSS, and SQLite. Implemented user authentication and home page where user can post job application or find job opportunities.",
       tech: ["Python", "HTML/CSS", "SQLite"],
-      image: "https://images.unsplash.com/photo-1557821552-17105176677c?w=800&h=500&fit=crop",
+      image: "/src/images/pijob.png",
       github: "https://github.com/AllenPetaluna/Professional-Networking-System"
     },
     {
       title: "Taskify",
       description: "Developed a task management tool with real-time updates using WebSockets. Features include drag-and-drop, team collaboration, and notifications.",
       tech: ["JavaScript","React", "Vite", "Java", "HTML/CSS", "MySQL"],
-      image: "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?w=800&h=500&fit=crop",
+      image: "/src/images/taskify.png",
       github: "https://github.com/felraine/Taskify-Project"
     },
     {
       title: "FilipinoExplorer",
       description: "Created an interactive weather dashboard that displays current conditions and forecasts using OpenWeather API with data visualization.",
       tech: ["React", "Vite","Chart.js", "API Integration"],
-      image: "https://images.unsplash.com/photo-1592210454359-9043f067919b?w=800&h=500&fit=crop",
+      image: "/src/images/filipino_explorers.png",
       github: "https://github.com/AbarquezMyk/Capstone_FilipinoExplorers"
     },
     {
       title: "E-Vents",
       description: "Created an interactive weather dashboard that displays current conditions and forecasts using OpenWeather API with data visualization.",
       tech: ["React", "Chart.js", "API Integration"],
-      image: "https://images.unsplash.com/photo-1592210454359-9043f067919b?w=800&h=500&fit=crop",
+      image: "/src/images/e-vents.jpg",
       github: "https://github.com/villamorbudz/E-vents"
     },
     {
       title: "Quizzard",
       description: "Created an interactive weather dashboard that displays current conditions and forecasts using OpenWeather API with data visualization.",
       tech: ["React", "Chart.js", "API Integration"],
-      image: "https://images.unsplash.com/photo-1592210454359-9043f067919b?w=800&h=500&fit=crop",
+      image: "https://via.placeholder.com/800x500/808080/808080",
       github: "#"
     }
   ];
